0f66977a673335eeccafd669670709f9d053bf15
Shubham Saxena <shubh3695@gmail.com>
2017-10-01 20:06:02 +0530

    Updated Allotment Info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/app.headerComponent';
 import { NavComponent } from './nav/app.navComponent';
